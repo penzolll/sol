c66dc6c6a1093aff0e9cbe80e204474ce4526174
penzolll <matdew444@outlook.com>
2026-08-14 22:44:34 +0700

    Update server response message and log output

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('gmgn-cli is running on Railway\n');
+  res.end('gmgn-cli is alive\n');
 }).listen(port, '0.0.0.0', () => {
-  console.log(`Server listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
